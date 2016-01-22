58dbf644f5de5db7cc4c1dd5eb63045995dfc6f7
houpanqi <houpanqi@qq.com>
2016-01-22 20:00:08 +0800

    update js

--- phone/js/regist.js
+++ phone/js/regist.js
@@ -6,26 +6,26 @@ var register = {
         password:'',
         rpassword:'',
         captcha:'',
-        regBtn:'',
+        submitBtn:'',
     },
     //初始化绑定事件
     init:function(){
         //绑定mobile
         $(register.options.mobile).on('blur',function(){
-            register.highLight($(this),filters.mobile($(this).val()));  
+            register.checkForm($(this),filters.mobile($(this).val()));  
         });
         //绑定密码
         $(register.options.password).on('blur',function(){
-             register.highLight($(this),filters.password($(this).val()));
+             register.checkForm($(this),filters.password($(this).val()));
         });
         //绑定重复密码
         $(register.options.rpassword).on('blur',function(){
-             register.highLight($(this),filters.rpassword($(this).val()));
+             register.checkForm($(this),filters.rpassword($(this).val()));
         });
-    },
-    //检查表单元素是否符合要求
-    checkForm:function(mobile,password,rpassword,yzm){
-        
+        //绑定注册按钮
+        $(register.options.submitBtn).on('blur',function(){
+             
+        });
     },
     //发送短信验证码
     sendSms:function(){
@@ -35,14 +35,19 @@ var register = {
     checkSmsCaptcha:function(captcha){
         
     },
-    //高亮提醒,dotype @get true,false
-    highLight:function(o,dotype){
+    //检查表单元素是否符合要求,并高亮提醒,dotype @get true,false
+    checkForm:function(o,dotype){
         if(dotype){
             o.removeClass('highlight');
+            return true;
         }else{
             o.addClass('highlight');
+            return false;
         }
     },
+    submitForm:function(){
+        
+    }
 };
 
 //表单滤镜
