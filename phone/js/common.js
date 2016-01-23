649fd1cc4ab03e6467e54ec2040d8fa385c991a3
wodrow <wodrow451611cv@gmail.com>
2016-01-23 17:29:18 +0800

    update

--- phone/js/common.js
+++ phone/js/common.js
@@ -1,13 +1,48 @@
 var Tools = {
 
     //纯静态tab切换
-    setTab:function(name,cursel,n){
-        for(i=1;i<=n;i++){
-            var menu=document.getElementById(name+i);
-            var con=document.getElementById("con_"+name+"_"+i);
-            menu.className=i==cursel?"hover":"";
-            con.style.display=i==cursel?"block":"none";
+    setTab: function (name, cursel, n) {
+        for (i = 1; i <= n; i++) {
+            var menu = document.getElementById(name + i);
+            var con = document.getElementById("con_" + name + "_" + i);
+            menu.className = i == cursel ? "hover" : "";
+            con.style.display = i == cursel ? "block" : "none";
         }
-    }
+    },
 
+    //html转义解析
+    html_encode: function (str) {
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&/g, "&gt;");
+        s = s.replace(/</g, "&lt;");
+        s = s.replace(/>/g, "&gt;");
+        s = s.replace(/ /g, "&nbsp;");
+        s = s.replace(/\'/g, "&#39;");
+        s = s.replace(/\"/g, "&quot;");
+        s = s.replace(/\n/g, "<br>");
+        return s;
+    },
+    html_decode: function (str) {
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&gt;/g, "&");
+        s = s.replace(/&lt;/g, "<");
+        s = s.replace(/&gt;/g, ">");
+        s = s.replace(/&nbsp;/g, " ");
+        s = s.replace(/&#39;/g, "\'");
+        s = s.replace(/&quot;/g, "\"");
+        s = s.replace(/<br>/g, "\n");
+        return s;
+    },
+    htmlEncode: function (str) {
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    },
+    htmlDecode: function (str) {
+        var div = document.createElement("div");
+        div.innerHTML = str;
+        return div.innerHTML;
+    }
 }
